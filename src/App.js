4ed187e2d5b3fc0efd55e46c5165c6e03c420718
vijaykumar <vijay10022@gmail.com>
2020-04-21 21:58:30 +0530

    did

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       </header><br></br><br></br>
         <Route exact path="/profile" component={Profile}>
         </Route>
-        <Route exact path="/mydemo" component={home}></Route>
+        <Route exact path="/demo" component={home}></Route>
         </BrowserRouter>       
         </section>
     )
